fc2d219765bcd324e854a911b04fbc662e23f536
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-07 19:43:58 -0500

    ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,17 @@ class GamesController < ApplicationController
   end
 
   def score
+    attempt = params[:word]
+    letters = params[:letters]
+    @message = ""
+    if !word_in_grid?(attempt, letters)
+      @message = "The word does not exist in the grid"
+    elsif word_in_grid?(attempt, letters) && !english_word?(attempt)
+      @message = "The word must be an english word!!"
+    else
+      @message = "You win!!"
+    end
+    @message
   end
 
   private
@@ -21,19 +32,19 @@ class GamesController < ApplicationController
     grid.shuffle
   end
 
-  def word_in_grid?(attempt)
+  def word_in_grid?(attempt, grid)
     # El intento lo pasamos a array y con el enumerable all?
     # pasamos cada letra por el bloque donde se ejecuta una condicion
     # Con count se suma el numero de veces que aparece la letra y se compara con
     # el numero de veces que aparece en la grid dada. Si todos estos pasan all? devuelve
     # true, de lo contrario devuelve false
-    attempt.chars.all? { |letter| attempt.count(letter) <= @letters.count(letter) }
+    attempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
   end
 
   def english_word?(attempt)
     # Consultamos si la palabra ingresada es una palabra en ingles
     url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
-    query = URI.parse(url).open
+    query = URI.open(url).read
     result = JSON.parse(query)
     result['found']
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/new' => 'games#new'
   post '/score' => 'games#score'
+  get '/score' => 'games#score'
 end
